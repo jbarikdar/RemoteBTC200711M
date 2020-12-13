36b3573ff1bb607d7c1c79f1a0a24462f6c4a428
manna <j.barikdar@yahoo.com>
2020-12-13 14:20:49 -0500

    added project

---
+++ TestNGProject/src/test/java/com/bit/test/DataDrivenTest.java
@@ -0,0 +1,9 @@
+package com.bit.test;
+
+import org.testng.annotations.Test;
+
+public class DataDrivenTest {
+	  @Test(dataProvider = "dp")
+	  public void f(Integer n, String s) {
+	  }
+}
---
+++ TestNGProject/src/test/java/com/bit/test/Regression.java
@@ -0,0 +1,14 @@
+package com.bit.test;
+
+import org.testng.annotations.Test;
+
+public class Regression extends BaseTest{
+	@Test
+	public void test2() {
+		System.out.println("Test2");
+	}	
+	@Test
+	public void test3() {
+		System.out.println("Test3");
+	}
+}
